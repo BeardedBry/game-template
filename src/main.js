0e3db6808804eb60d96230e264d67e2c1bdf5e1a
Brian Burkard <brianburkard@gmail.com>
2020-09-10 00:29:21 -0400

    rick clicker

--- src/main.js
+++ src/main.js
@@ -5,4 +5,40 @@ let startTime = Date.now();
 function sync(dom, pos) {
     dom.style.left = `${pos.x}px`;
     dom.style.top = `${pos.y}px`;
+    dom.style.width = '5rem';
+    //dom.style.height = '5rem';
+}
+
+function addClicker(){
+    const pos = {
+        x: Math.random() * 500,
+        y: Math.random() * 300
+    };
+
+    const img = new Image();
+    img.src = "res/images/rick.png";
+    img.style.position = "absolute";
+    img.addEventListener("click", removeClicker, false);
+
+    document.querySelector("#board").appendChild(img);
+    sync(img, pos);
+}
+
+function removeClicker(e) {
+    e.target.parentNode.removeChild(e.target);
+    clickers--;
+    checkGameOver();
+}
+
+function checkGameOver(){
+    document.querySelector('#remain').innerHTML = clickers;
+    if (clickers === 0){
+        const taken = Math.round((Date.now() - startTime) / 1000);
+        alert(`De-ricked in ${taken} seconds!`);
+    }
+}
+
+// start game
+for(let i = 0; i < clickers; i++){
+    addClicker();
 }
