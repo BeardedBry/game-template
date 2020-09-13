a16467c65c7fac4685143e3afc7fb374515143fe
Brian Burkard <brianburkard@gmail.com>
2020-09-13 15:00:58 -0400

    draw text

--- src/main.js
+++ src/main.js
@@ -6,22 +6,17 @@ const ctx = canvas.getContext("2d");
 
 const {width: w, height: h} = canvas;
 
-ctx.fillStyle = "black";
+ctx.fillStyle = "grey";
 ctx.fillRect(0, 0, w, h);
-ctx.fillStyle = "#555";
 
-let x, y, radius;
-
-for(let i = 0; i < 550; i++) {
-    x = Math.random() * w;
-    y = Math.random() * h;
-    radius = Math.random() * 3;
-    
-    ctx.beginPath();
-    ctx.arc(x, y, radius, 0, Math.PI * 2, false);
-    ctx.fill();
+const center = w/2;
+ctx.textAlign = "center";
+ctx.fillStyle =  "black";
+for(let i = 1; i < 11; i++){
+    ctx.fillText("text in game", center, i * 40);
 }
 
+ctx.strokeText("stroked text", center, h-30);
 
 // const gridSize = 10;
 // const grid = [];
