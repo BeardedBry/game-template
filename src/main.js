9728104728137550e2700f780c11e344b12e7e50
Brian Burkard <brianburkard@gmail.com>
2020-09-13 14:34:03 -0400

    cosmic starfield

--- src/main.js
+++ src/main.js
@@ -4,19 +4,24 @@ const ctx = canvas.getContext("2d");
 // const deg2Rad = deg => (Math.PI / 180) * deg;
 // const rad2Deg = rad => (180 / Math.PI) * rad;
 
-ctx.strokeStyle="black";
-ctx.fillStyle="red";
+const {width: w, height: h} = canvas;
 
-ctx.fillRect(300, 200, 50, 50);
-ctx.strokeRect(300,200,50,50);
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, w, h);
+ctx.fillStyle = "#555";
 
-ctx.beginPath();
-ctx.arc(325, 170, 25, 0, Math.PI * 2, false);
-ctx.fill();
-ctx.stroke();
+let x, y, radius;
 
+for(let i = 0; i < 550; i++) {
+    x = Math.random() * w;
+    y = Math.random() * h;
+    radius = Math.random() * 3;
+    
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, Math.PI * 2, false);
+    ctx.fill();
 
-
+}
 
 
 // const gridSize = 10;
